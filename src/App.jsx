cfa79f7b9e9bb05ffa2627ca2c4a36b994642f05
Sohel Rana <mirzasohel550@gmail.com>
2025-01-03 03:00:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../css/style.css"
+import Header from './components/Header';
+
+
+const App = () => {
+  return (
+    <div>
+     <Header/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+               <div className="container">
+               <nav>
+                <img className='developer' src="/src/assets/images/developer (2).png" alt="" />
+                     
+                     <h2>Ma<span>r</span>y</h2>
+                    <ul>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <button>Hire Me</button>
+                    </ul>
+                </nav>
+                <div className='header-text'>
+                      <div className='header-left'>
+                      <h2>Hi, I  am</h2>
+                      <h1>Mary Hardy</h1>
+                      <p>Shot what able cold new see hold.Friendly as an betrayed formerly he. Morning because as to society behaved moments.</p>
+                      <button className='down-btn'>Download CV</button>
+                      <button className='contact-btn'>Contact</button>
+                      </div>
+                      <div className='header-right'>
+                            <img className='shape' src="/src/assets/images/shape.png" alt="" />
+                            <img className='man' src="/src/assets/images/man.png" alt="" />
+                      </div>
+
+                </div>
+               </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
